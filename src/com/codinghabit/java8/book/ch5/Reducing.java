85764840229f352e76d9eeb410350280ed349c04
Marcos Savoury <marcossavoury@hotmail.com>
2015-08-31 09:17:17 -0400

    Adding another example of reducing

--- src/com/codinghabit/java8/book/ch5/Reducing.java
+++ src/com/codinghabit/java8/book/ch5/Reducing.java
@@ -2,6 +2,7 @@ package com.codinghabit.java8.book.ch5;
 
 import com.codinghabit.java8.book.Apple;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,7 +10,9 @@ import java.util.List;
  */
 public class Reducing {
     public static void main(String [] args) {
+
         reduceApple();
+        reduceAsSum();
     }
 
     public static void reduceApple() {
@@ -17,4 +20,15 @@ public class Reducing {
         Apple superApple = appleList.stream().reduce(new Apple(),(a, b) -> new Apple(a.getWeight()+b.getWeight(), "super"));
         System.out.println(superApple);
     }
+
+    /*
+     * Take a list of ints and reduce to a sum of the elements
+     */
+    public static void reduceAsSum() {
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        int sum = myInts.stream().reduce(0, (a, b) -> a + b);
+        System.out.println("sum is " + sum);
+    }
+
+
 }
