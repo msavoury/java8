55c13485d44481f662df5a9700f155127b85a342
Marcos Savoury <marcossavoury@hotmail.com>
2015-09-05 13:23:22 -0400

    More streams

--- src/com/codinghabit/java8/book/ch5/GeneratingStreams.java
+++ src/com/codinghabit/java8/book/ch5/GeneratingStreams.java
@@ -1,10 +1,11 @@
 package com.codinghabit.java8.book.ch5;
 
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
- * This class provides examples of generarting streams
+ * This class provides examples of generating streams
  * in various ways
  *  */
 public class GeneratingStreams {
@@ -22,5 +23,9 @@ public class GeneratingStreams {
 
         Stream<Integer> integerStream = Stream.of(1,2,3,4,5,6);
         integerStream.forEach(printer);
+
+        IntStream myStream = IntStream.of(1,2,3,4,5,6,7,8,9,10);
+        myStream.map(i -> (i * 4));
+        myStream.forEach(System.out::println);
     }
 }
